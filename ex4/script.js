f96a13e042621c9848e8aff213723184dbd1c3e8
Larissa Alves Brabosa <lymim4888@gmail.com>
2026-05-26 08:25:37 -0300

    Update script.js

--- ex4/script.js
+++ ex4/script.js
@@ -1,12 +1,11 @@
 const listaNumeros = [];
 const listaSemDuplicados = [];
 
-// Ler 10 números
 for (let i = 0; i < 10; i++) {
     listaNumeros.push(Number(prompt(`Digite o ${i + 1}º número:`)));
 }
 
-// Remover duplicados
+
 for (let i = 0; i < listaNumeros.length; i++) {
 
     let numero = listaNumeros[i];
@@ -16,7 +15,7 @@ for (let i = 0; i < listaNumeros.length; i++) {
     }
 }
 
-// Mostrar resultado
+
 alert(
     "Vetor Original: " + listaNumeros.join(", ") +
     "\nVetor Sem Duplicatas: " + listaSemDuplicados.join(", ")
